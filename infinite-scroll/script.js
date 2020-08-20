5092bf08f023890d506451aa1420b2a68bd89c4f
cdsalty <soltiscd@gmail.com>
2020-08-20 18:07:07 -0400

    redering dynamic results; next step is to add infinite scrolling ability with autorefresh

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -1,6 +1,7 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+const count = 10; // represents how many pictures to return/display.  
 // variable for photosArray
 let photosArray = [];
 
@@ -8,25 +9,43 @@ let photosArray = [];
 const apiKey = 'k59C4PPe8QTSxOUWuFR8a3ZSVpojkfURvsQX-4V4VyI';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-const count = 10; // represents how many pictures to return/display.  
+// HELPER FUNCTION (to make code cleaner and DRY)
+const setAttributes = (element, attributes) => {
+  // FOR EACH INDIVIDUAL KEY IN THAT IS INSIDE OF ATTRIBUTES, return the key and the attribute TO THAT SPECIFIC KEY. 
+  for (const key in attributes) {
+    // element.setAttribute(key, attributes[key])
+    return element.setAttribute(key, attributes[key])
+  }
+}
+
 
 // Create Elements for Links & Photos; Add to Dom
-const displayPhotos = () => {
+function displayPhotos() {
   photosArray.forEach((photo) => {
     // create <anchor> element to link to Unsplash
     const item = document.createElement('a'); // creates a blank anchor element
-    // add attributes to item
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank'); // will open in a new window
-
+    // set and add attributes to item
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    // item = <a href = "'photo.links.html' tartget='_blank'">
     // Create <img> for photo
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description
+    });
+    // setAttributes function removes the unnecesary code below.
+    // img.setAttribute('src', photo.urls.regular);
+    // img.setAttribute('alt', photo.alt_description);
+    // img.setAttribute('title', photo.alt_description);
+    // <img src = "photo.urls.regular" alt = "photo.alt_description" title="photo.alt_description" >
 
     // Put <img> inside <a>; then put inside imageContainer Element
-
+    item.appendChild(img);  // 1st: take the item varaible(anchor tag) and append to it the image element
+    imageContainer.appendChild(item); // 2nd: add the item element created to the DOM inside the imageContainer class
   });
 }
 
