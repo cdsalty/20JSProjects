e2b38b96ac3a906fc87c2cf9453ba0ce2d61649e
cdsalty <soltiscd@gmail.com>
2020-09-11 13:00:35 -0400

    working on light and dark functions for calling

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -1,20 +1,38 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]'); // in order to add event listener to check when the switch is on or off. 
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
 
-
-// Switch Teme Functionality:
-const switchTheme = (event) => {
-  if (event.target.checked) {
-    // https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement
-    document.documentElement.setAttribute('data-theme', 'dark')
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light')
-  }
-  console.log(event); // confirmed that I am getting the event and I need the target property which has the checked property as true
-  console.log(event.target.checked);
+// Dark Mode Function
+const darkMode = () => {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  console.log(toggleIcon.children);
 }
 
-// Event Listener:
+
+// Switch Teme Functionality: making use of 'setAttribute' (https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement)
+const switchTheme = (event) => {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    darkMode();
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    lightMode();
+  }
+  console.log(event); // confirmed that I am getting the event; I need to target property, which is checked as true or false;
+  // console.log(event.target.checked);
+}
+
+
 toggleSwitch.addEventListener('change', switchTheme)
 
 // If checked yes, we want dark mode: (https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement)
 
+/*
+Need to use switchTheme function to call two separate functions: one for dark mode and one for light mode
+
+*/
