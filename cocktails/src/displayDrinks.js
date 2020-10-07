2876a839d75dfecda621ba9b57b792d1c07e3a24
cdsalty <soltiscd@gmail.com>
2020-10-07 11:55:08 -0400

    getting list of cocktails array

--- cocktails/src/displayDrinks.js
+++ cocktails/src/displayDrinks.js
@@ -1,7 +1,37 @@
 import get from './getElement.js';
 
-const displayDrinks = ({ drinks }) => {
-  return "hello world";
+const displayDrinks = ({ drinks }) => { // or data and then will have to further write data.drinks
+  const section = get('.section-center');
+  const title = get('.title');
+  // console.log(drinks);
+  if (!drinks) {
+    // hide loading
+    title.textContent = "Sorry, No drinks matched your search; Please try again.";
+    section.innerHTML = null;
+    return;
+  }
+
+  const newDrinks = drinks.map((drink) => {
+    return `<a href="drink.html">
+        <article class="cocktail" data-id="1">
+          <img src="./cocktail.jpg" alt="cocktail" />
+          <h3>martini</h3>
+        </article>
+      </a>`
+  }).join(" ");
+  // hide loading
+  title.textContent = "";
+  section.innerHTML = newDrinks;
+  // return section;
 }
 
 export default displayDrinks;
+
+
+/*]
+
+In this function, what matters the most is the 'drinks' variable we are passing in.
+  - Drinks will be an array or null;
+
+
+*/
--- cocktails/src/presentDrinks.js
+++ cocktails/src/presentDrinks.js
@@ -1,14 +1,13 @@
 import fetchDrinks from './fetchDrinks.js';
 import displayDrinks from './displayDrinks.js';
 
-// takes any url, and if the correct url is used, then this is what I expect. 
 const showDrinks = async (url) => {
   // fetch drinks
   const data = await fetchDrinks(url);
   // display drinks
-  // console.log(data);
-  const section = await displayDrinks(data);  // go create a function that displays drinks
-  console.log(section)
+  const section = await displayDrinks(data);  //function that displays drinks
+  console.log(section);
+
 }
 
 export default showDrinks;
