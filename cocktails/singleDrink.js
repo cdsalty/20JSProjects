9960bd700b84abef34e3cf1ea340f1c5945e26b0
cdsalty <soltiscd@gmail.com>
2020-10-14 11:12:01 -0400

    project completed

--- cocktails/singleDrink.js
+++ cocktails/singleDrink.js
@@ -4,13 +4,17 @@
 import fetchDrinks from './src/fetchDrinks.js';
 import displayDrink from './src/displaySingleDrink.js';
 
-// *** THE REAL GOAL HERE!!!! ****  ---> to retrieve the id being stored into local-storage and pass it into the new url
+// *** THE REAL GOAL!!!! ****  ---> to retrieve the id being stored into local-storage and pass it into the new url
 const presentDrink = async () => {
   // get id from local-storage
   const id = localStorage.getItem('drink');
-  console.log({ id })
-  const drink = await fetchDrinks(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-  displayDrink(drink);
+  // console.log({ id })
+  if (!id) {
+    window.location.replace('index.html') // go back to home page
+  } else {
+    const drink = await fetchDrinks(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+    displayDrink(drink);
+  }
 }
 // IMPORTAnT to remember
 window.addEventListener('DOMContentLoaded', presentDrink)
